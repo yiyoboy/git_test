0848c7f05bd6f09781b0e5366bd43a77b852fa97
liudawei <liudawei@bonc.com.cn>
2019-07-18 09:52:54 +0800

    vue

--- 8.class.js
+++ 8.class.js
@@ -1,1 +1,12 @@
 //类
+class Person {
+    constructor(name){
+        this.name=name; //实例的私有属性
+    }
+    getName(){
+        console.log(this.name)
+    }
+}
+
+let p = new Person('liu1111');
+p.getName()
